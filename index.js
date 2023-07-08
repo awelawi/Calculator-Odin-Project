5a8179610a87ead15a24c9036e55c60f7557a66f
Ella <emmanuellaumoye18@gmail.com>
2023-07-08 13:33:04 -0700

    Semi functional calculator. Users can now make first operations, however, successive computations are not being calculated correctly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let parent_container = document.querySelector("#parent-container");
 let display = document.getElementById("display");
 let clear_btn = document.querySelector("#clear");
-let delete_btn = document.querySelector("delete");
+let delete_btn = document.querySelector("#delete");
 let one_btn = document.querySelector("#the-one");
 let two_btn = document.querySelector("#the-two");
 let three_btn = document.querySelector("#the-three");
@@ -19,6 +19,8 @@ let dot_btn = document.querySelector("#the-dot");
 let equals_btn = document.querySelector("#the-equals");
 let divide_btn = document.querySelector("#the-divide");
 console.log(display);
+let number_str = "";
+let number_str_2 = "";
 
 let display_value = "";
 const numbers_btns = document.querySelectorAll(".number-buttons");
@@ -26,92 +28,129 @@ const operator_btns = document.querySelectorAll(".operators-buttons");
 let operator_and_number = {
     num_1: 0,
     num_2: 0,
-    operator: ""
+    operator: 0,
+    result: 0
 };
-
+clear_btn.addEventListener("click", handlerClear);
+delete_btn.addEventListener("click", handleDelete);
 numbers_btns.forEach(button => {
     button.addEventListener("click", () => {
-        if(true){
-            operator_and_number.num_1 = button.textContent;
-        }
-        else{
-            operator_and_number.num_2 = button.textContent;
-        }
         display_value = button.textContent;
-        console.log(typeof(display_value))
-        display.innerHTML += `${display_value}`;
+        display.innerHTML += display_value;
+        if (isOperator()){
+            console.log("num_2 is pressed");
+            console.log("button textContent: ", button.textContent);
+            number_str_2 += button.textContent;
+            console.log("num_2: ", number_str_2);
+            operator_and_number.num_2 = Number(number_str_2);
+            console.log(operator_and_number.num_2)
+        }
+
+        else if(!isOperator()){
+            console.log("num_1 is pressed");
+            console.log("button textContent: ", button.textContent);
+            number_str += button.textContent; 
+            operator_and_number.num_1 = Number(number_str);
+            console.log(operator_and_number.num_1)
+        }
     })
-});
+}); 
 
 operator_btns.forEach(button => {
     button.addEventListener("click", () => {
         display_value = button.textContent
         display.innerHTML += display_value
+        operator_and_number.operator = button.textContent;
     })
 });
 
+equals_btn.addEventListener("click", handleOperator)
 
 /**
  * Checks whether the textarea is empty or not
  * TBD: This shpuld actuallyt check whether an operator exists on the textarea or not, to determine if we're on
  * num_1 or num_2
  */
-function isDisplay(){
-    //use pattern match to determine whether an operation is on the display
-    const regex = /\+\*=-\//i;
-    console.log(regex)
-    console.log(regex.test(display))
-    if (regex.test(display) == true){
-        console.log("the operator btn has been clicked");
+function isOperator(){
+    //if the operator button is checked, return true
+    if(operator_and_number.operator != null){
         return true;
     }
     return false;
 }
 
-isDisplay();
+//isDisplay();
 
-function receiveInput(){
+function handleOperator () {
+    // Get the clicked operator button
 
+    // Get the operator value from the button
+    const operator = operator_and_number.operator;
+
+    // Perform an action based on the operator
+    switch (operator) {
+        case "+":
+            console.log("operators: ", operator_and_number);
+            operator_and_number.result = add(operator_and_number.num_1, operator_and_number.num_2);
+            console.log(operator_and_number.result);
+            break;
+        case "-":
+            operator_and_number.result = subtract(operator_and_number.num_1, operator_and_number.num_2);
+            break;
+        case "*":
+            console.log("operators: ", operator_and_number);
+            operator_and_number.result = multiply(operator_and_number.num_1, operator_and_number.num_2);
+            console.log(operator_and_number.result);
+
+            break;
+        case "/":
+            operator_and_number.result = divide(operator_and_number.num_1, operator_and_number.num_2);
+            break;
+        default:
+            // Handle other operators or unknown cases
+            break;
+    }
+    isResult();
 }
-function operate(num_1, num_2, operator){
-    if (operator === "+"){
-        add(num_1, num_2);
-    }
 
-    else if(operator === "-"){
-        subtract(num_1 - num_2);
-    }
-
-    else if(operator === "*"){
-        multiply(num_1 * num_2);
-    }
-
-    else{
-        if(num_2 === 0){
-            alert("invalid operation");
-        }
-        else{
-            divide(num_1, num_2)
-        }
+function isResult(){
+    if (operator_and_number.result != null){
+        display.textContent = operator_and_number.result;
     }
 }
 
+function handlerClear(){
+    operator_and_number.num_1 = 0;
+    operator_and_number.num_2 = 0;
+    operator_and_number.operator = 0;
+    operator_and_number.result = 0;
+    display.innerHTML = "";
+}
+
+function handleDelete(){
+    let curr_display = display.textContent;
+    console.log(curr_display);
+    display.textContent = curr_display.slice(0, curr_display.length-1);
+}
 function add(x, y){
-    return display_value += `${x + y}`
+    console.log("adding");
+    console.log("x: ", x);
+    console.log("y: ", y);
+    console.log("x+y: ", x + y);
+    return display_value = `${x + y}`;
 }
 
 function subtract(x, y){
-    return display_value += `${x - y}`
+    console.log("subtracting");
+    return display_value = `${x-y}`;
 }
 
 function divide(x, y){
-    return display_value += `${x / y}`
+    console.log("dividing");
+    return display_value = `${x / y}`;
 }
 
 function multiply(x, y){
-    return display_value += `${x * y}`
-}
-
-function popTextArea(value_to_display){
-    return display.textContent += `${value_to_display}`;
+    console.log("multiplying");
+    return display_value = `${x * y}`;
 }
