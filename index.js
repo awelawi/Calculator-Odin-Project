920348d1168e8f532d591ed7ba98dc7b1883ee94
Ella <emmanuellaumoye18@gmail.com>
2023-07-08 14:18:41 -0700

    calculator functionality is semi-finished. User can now perform operations on TWO NUMBERS ONLY. Cleaned up code by removing debug comments and refactored variable names. TODO: Users shiuld be able to perform multiple operations of the same or different types in one sitting

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
+/**
+ * Defining variables to manipulate the DOM
+ */
 let parent_container = document.querySelector("#parent-container");
 let display = document.getElementById("display");
 let clear_btn = document.querySelector("#clear");
 let delete_btn = document.querySelector("#delete");
-let one_btn = document.querySelector("#the-one");
-let two_btn = document.querySelector("#the-two");
-let three_btn = document.querySelector("#the-three");
-let add_btn = document.querySelector("#the-add");
-let four_btn = document.querySelector("#the-four");
-let five_btn = document.querySelector("#the-five");
-let six_btn = document.querySelector("#the-six");
-let subtract_btn = document.querySelector("#the-subtract");
-let seven_btn = document.querySelector("#the-seven");
-let eight_btn = document.querySelector("#the-eight");
-let nine_btn = document.querySelector("#the-nine");
-let multiply_btn = document.querySelector("#the-star");
-let zero_btn = document.querySelector("#the-zero");
-let dot_btn = document.querySelector("#the-dot");
 let equals_btn = document.querySelector("#the-equals");
-let divide_btn = document.querySelector("#the-divide");
-console.log(display);
-let number_str = "";
-let number_str_2 = "";
-
+let digit_1 = "";
+let digit_2 = "";
 let display_value = "";
 const numbers_btns = document.querySelectorAll(".number-buttons");
 const operator_btns = document.querySelectorAll(".operators-buttons");
+
+/**
+ * An object that holds tje numbers to be operated on, operation and result
+ * returns None
+ */
 let operator_and_number = {
     num_1: 0,
     num_2: 0,
-    operator: 0,
+    operator: null,
     result: 0
 };
-clear_btn.addEventListener("click", handlerClear);
+
+clear_btn.addEventListener("click", handleClear);
 delete_btn.addEventListener("click", handleDelete);
+
+/**
+ * For each number button pressed, we need to determine if it's part of number 1 or part of number 2
+ */
 numbers_btns.forEach(button => {
     button.addEventListener("click", () => {
         display_value = button.textContent;
         display.innerHTML += display_value;
         if (isOperator()){
-            console.log("num_2 is pressed");
-            console.log("button textContent: ", button.textContent);
-            number_str_2 += button.textContent;
-            console.log("num_2: ", number_str_2);
-            operator_and_number.num_2 = Number(number_str_2);
-            console.log(operator_and_number.num_2)
+            digit_2 += button.textContent;
+            operator_and_number.num_2 = Number(digit_2);
         }
 
         else if(!isOperator()){
-            console.log("num_1 is pressed");
-            console.log("button textContent: ", button.textContent);
-            number_str += button.textContent; 
-            operator_and_number.num_1 = Number(number_str);
-            console.log(operator_and_number.num_1)
+            digit_1 += button.textContent; 
+            operator_and_number.num_1 = Number(digit_1);
         }
     })
 }); 
@@ -67,10 +56,10 @@ operator_btns.forEach(button => {
 equals_btn.addEventListener("click", handleOperator)
 
 /**
- * Checks whether the textarea is empty or not
- * TBD: This shpuld actuallyt check whether an operator exists on the textarea or not, to determine if we're on
- * num_1 or num_2
- */
+ * Checks whether an operator has been clicked
+ * @returns boolean
+ * 
+*/
 function isOperator(){
     //if the operator button is checked, return true
     if(operator_and_number.operator != null){
@@ -79,28 +68,46 @@ function isOperator(){
     return false;
 }
 
-//isDisplay();
+/**
+ * Resets the calculator whenever the user presses the clear button
+ * @returns None
+ */
+function handleClear () {
+    operator_and_number.num_1 = 0;
+    operator_and_number.num_2 = 0;
+    operator_and_number.operator = null;
+    operator_and_number.result = 0;
+    display.innerHTML = "";
+    digit_1 = "";
+    digit_2 = "";
+}
 
+/**
+ * Should delete the last digit entered on the screen and update the calculated values as needed
+ * returns None
+ */
+function handleDelete () {
+    let curr_display = display.textContent;
+    display.textContent = curr_display.slice(0, curr_display.length - 1);
+}
+
+/**
+ * Invokes one of four of the operation functions whenever an operation button is clicked.
+ * @returns None
+ */
 function handleOperator () {
-    // Get the clicked operator button
-
-    // Get the operator value from the button
     const operator = operator_and_number.operator;
 
     // Perform an action based on the operator
     switch (operator) {
         case "+":
-            console.log("operators: ", operator_and_number);
             operator_and_number.result = add(operator_and_number.num_1, operator_and_number.num_2);
-            console.log(operator_and_number.result);
             break;
         case "-":
             operator_and_number.result = subtract(operator_and_number.num_1, operator_and_number.num_2);
             break;
         case "*":
-            console.log("operators: ", operator_and_number);
             operator_and_number.result = multiply(operator_and_number.num_1, operator_and_number.num_2);
-            console.log(operator_and_number.result);
 
             break;
         case "/":
@@ -113,44 +120,53 @@ function handleOperator () {
     isResult();
 }
 
+/**
+ * Displays the result in the text area
+ * @returns void
+ */
 function isResult(){
     if (operator_and_number.result != null){
         display.textContent = operator_and_number.result;
     }
 }
 
-function handlerClear(){
-    operator_and_number.num_1 = 0;
-    operator_and_number.num_2 = 0;
-    operator_and_number.operator = 0;
-    operator_and_number.result = 0;
-    display.innerHTML = "";
-}
-
-function handleDelete(){
-    let curr_display = display.textContent;
-    console.log(curr_display);
-    display.textContent = curr_display.slice(0, curr_display.length-1);
-}
+/**
+ * Adds two numbers together
+ * @param {A} x 
+ * @param {*} y 
+ * @returns 
+ */
 function add(x, y){
-    console.log("adding");
-    console.log("x: ", x);
-    console.log("y: ", y);
-    console.log("x+y: ", x + y);
+
     return display_value = `${x + y}`;
 }
 
+/**
+ * Subtracts one number from another
+ * @param {*} x 
+ * @param {*} y 
+ * @returns 
+ */
 function subtract(x, y){
-    console.log("subtracting");
     return display_value = `${x-y}`;
 }
 
+/**
+ * Divides two numbers
+ * @param {*} x 
+ * @param {*} y 
+ * @returns 
+ */
 function divide(x, y){
-    console.log("dividing");
     return display_value = `${x / y}`;
 }
 
+/**
+ * Multiplies two numbers
+ * @param {*} x 
+ * @param {*} y 
+ * @returns 
+ */
 function multiply(x, y){
-    console.log("multiplying");
     return display_value = `${x * y}`;
 }
